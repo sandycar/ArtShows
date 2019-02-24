a5da3f7791abaa99e7f016c8731109899512c6ca
Julio Guereca <jguereca@bigpanda.io>
2019-02-23 21:08:33 -0800

    Rearranged code and added result div

--- assets/javascript/artshow.js
+++ assets/javascript/artshow.js
@@ -1,12 +1,102 @@
-// Variables
+// Variables that won't change you want to use the 'const' key word
+const clientID = '924751e8b551e9ebd791';
+const clientSecret = '2c10e18dafc2d9d8f18f57315157e2f6';
 
-// functions
-// console.log("hello")
+let token;
+getToken();
 
-var clientID = '924751e8b551e9ebd791',
-  clientSecret = '2c10e18dafc2d9d8f18f57315157e2f6'
+$('#searchForm').on('submit', function(){
+	event.preventDefault()
+	
+	var city = $('#searchForm #userInput').val()
+	
+	console.log(city)
+	search(city);	
+})
 
-var token;
+/**
+* Gets token to for artsy API requests.
+* @return {String} token - token for API requests.
+*/
+function getToken(){
+	$.ajax({
+		url:"https://api.artsy.net/api/tokens/xapp_token?client_id="+clientID+"&client_secret="+clientSecret,
+		method:"POST"
+	})
+	.then(function(data){
+		token = data.token;
+	})
+}
+
+/**
+* Will search value from inout form.
+* @param {String} q - city to search 
+*/
+function search(q){
+	$.ajax({
+		url:"https://api.artsy.net/api/search?q="+q,
+		method:"GET",
+		headers: { "X-Xapp-Token": token }
+	})
+	.then( (response)=> {
+		// console.log(response)
+		// console.log(response._embedded.results[1])
+		// for (var i=0;i<results.length;i++){
+		// 	// var element = results[i]
+		// 	// console.log(element.type)
+		// 	// This is the same ^^ and as the below:
+		// 	console.log(results[i].type)
+		// }
+		// The above is almost the same as the function below:
+
+		let results=response._embedded.results
+		let filteredResults = results.filter( (element) => element.type === "show")
+
+		let results_element = document.getElementById("art-results");
+		results_element.classList.remove("uk-hidden");
+		
+		console.log(filteredResults)
+		show(filteredResults[1]._links.self.href)
+	})
+}
+
+/**
+* Gets additional details from the provided URL.
+* @param {String} url - URL from the show provided.
+*/
+function show(url){
+	console.log(url);
+	$.ajax({
+		url:url,
+		method:"GET",
+		headers: { "X-Xapp-Token": token }
+	})
+	.then( (answer) => {
+		console.log("[show]->",answer)
+		console.log(answer.status)
+		console.log(answer.start_at)
+		console.log(answer.name)
+		console.log(answer._links.permalink.href)
+		partner(answer._links.partner.href)
+	})
+}
+
+/**
+* Gets partner informations
+* @param {String} url - partner URL
+*/
+function partner(url){
+	console.log();
+	$.ajax({
+		url:url,
+		method:"GET",
+		headers: { "X-Xapp-Token": token }
+	})
+	.then( (answer) => {
+		console.log("[partner]->",answer)
+	})
+}
+
 //   apiUrl = 'https://api.artsy.net/api/shows/924751e8b551e9ebd791" -H "X-XAPP-Token:',
 //   xappToken;
 
@@ -33,50 +123,8 @@ var token;
 // })
 
 // curl -v -X POST "https://api.artsy.net/api/tokens/xapp_token?client_id=...&client_secret=..."
-function getToken(){
-	$.ajax({
-		url:"https://api.artsy.net/api/tokens/xapp_token?client_id="+clientID+"&client_secret="+clientSecret,
-		method:"POST"
-	})
-	.then(function(data){
-		// console.log(data);
-		// console.log(data.token)
-		token = data.token
-	})
-}
 
 // curl -v "https://api.artsy.net/api/search?q=Andy+Warhol" -H "X-XAPP-Token:..."
-
-
-function search(q){
-	$.ajax({
-		url:"https://api.artsy.net/api/search?q="+q,
-		method:"GET",
-		headers: { "X-Xapp-Token": token }
-	})
-	.then(function(response){
-		// console.log(response)
-		// console.log(response._embedded.results[1])
-		var results=response._embedded.results
-		// for (var i=0;i<results.length;i++){
-		// 	// var element = results[i]
-		// 	// console.log(element.type)
-		// 	// This is the same ^^ and as the below:
-		// 	console.log(results[i].type)
-		// }
-		// The above is almost the same as the function below:
-		var filteredResults = results.filter(function(element){
-			if (element.type != "show"){
-				return false
-			} else{ 
-				return true
-			}
-
-		})
-		console.log(filteredResults)
-		show(filteredResults[1]._links.self.href)
-	})
-}
 
  // Reference for current AJAX:
  // 			var tr = $('<tr>');
@@ -87,43 +135,3 @@ function search(q){
 
  //            tr.append(tdm, tdy, tda);
  //            tbody.append(tr);
-
-function show(url){
-	console.log(url)
-	$.ajax({
-		url:url,
-		method:"GET",
-		headers: { "X-Xapp-Token": token }
-	})
-	.then(function(answer){
-		console.log("[show]->",answer)
-		console.log(answer.status)
-		console.log(answer.start_at)
-		console.log(answer.name)
-		console.log(answer._links.permalink.href)
-		partner(answer._links.partner.href)
-	})
-}
-function partner(url){
-	console.log()
-	$.ajax({
-		url:url,
-		method:"GET",
-		headers: { "X-Xapp-Token": token }
-	})
-	.then(function(answer){
-		console.log("[partner]->",answer)
-
-	})
-}
-
-getToken()
-
-$('#searchForm').on('submit', function(){
-	event.preventDefault()
-	
-	var keyword = $('#searchForm #userInput').val()
-	
-	console.log(keyword)
-	search(keyword);	
-})
